0663213617eaf391097c1820b9f1db17f02328bf
Adhish22 <53336778+Adhish22@users.noreply.github.com>
2019-08-18 17:30:53 +0530

    added port address on app.js file

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/" , function(req, res){
 	res.sendFile(__dirname + "/index.html");
-} )
+});
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
 	console.log("Server is running on port 3000");
 });
