33c51d95b34186ced6b52967a20b601502b2dd7e
Adhish22 <53336778+Adhish22@users.noreply.github.com>
2019-08-24 16:18:11 +0530

    replace carousel with slider and ul icon change

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/" , function(req, res){
-	res.sendFile(__dirname + "/index.html");
+	res.render("index");
 });
 
 app.listen(process.env.PORT || 3000, function(){
