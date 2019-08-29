8af4f21228e307d39c352ba4519e73f4a7b6fc3a
Adhish Sharma <adhishsharma0@gmail.com>
2019-08-30 03:33:20 +0530

    add projects page with data entry

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get("/" , function(req, res){
 	res.render("index");
 });
 
+app.get("/projects", function(req, res){
+	res.render("projects");
+});
+
 app.listen(process.env.PORT || 3000, function(){
 	console.log("Server is running on port 3000");
 });
