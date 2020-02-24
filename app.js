eea9864ec1f21c824596ba0d95ae04b23a0355a9
Adhish Sharma <adhishsharma0@gmail.com>
2020-02-25 00:15:20 +0530

    ouath code fixed

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/rockmechanics",
+    callbackURL: "http://rockmechanics.net/auth/google/rockmechanics",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
